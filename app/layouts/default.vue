21213dfe21ed1b4938c41ab895b5767d3cc5bdf3
muhrezaalfiansyah <50980568+muhrezaalfiansyah@users.noreply.github.com>
2025-12-11 15:08:52 +0700

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,8 +7,7 @@
         <!-- offcanvas search -->
         <div class="offcanvas offcanvas-top" id="offCanvasSearch" data-bs-backdrop="static" tabindex="-1" aria-labelledby="offcanvasTopLabel">
             <div class="offcanvas-header">
-                <!-- <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
+                
             </div>
             <div class="offcanvas-body">
                 <div class="container d-flex align-items-center justify-content-start gap-3">

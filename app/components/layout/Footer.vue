b4af001d9d2264b1f0690096d321b8b539057f17
muhrezaalfiansyah <50980568+muhrezaalfiansyah@users.noreply.github.com>
2025-12-11 16:16:35 +0700

    update footer

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -5,10 +5,10 @@
             <div class="row">
 
                 <!-- Left Section -->
-                <div class="col-md-4 mb-4">
-                    <img src="https://medicastore.com/themes/blue/images/logo-dark-header-default.png" alt="medicastore" class="mb-3" style="max-width:180px;">
+                <div class="col-md-5 mb-4">
+                    <img src="/public/images/logo-medicastore-putih.svg" alt="medicastore" class="mb-3" style="max-width:180px;">
 
-                    <p class="small">
+                    <p class="body-2" style="max-width: 300px;">
                         Scan QR code atau download aplikasi Medicastore yang tersedia di iOS dan Android.
                     </p>
 
@@ -27,43 +27,57 @@
                 </div>
 
                 <!-- Middle Section 1 -->
-                <div class="col-md-3 mb-4">
-                    <h6 class="fw-bold mb-3">Medicastore</h6>
-                    <ul class="list-unstyled small">
-                        <li><a href="#" class="text-white text-decoration-none">Tentang Kami</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Syarat & Ketentuan</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Kebijakan & Privasi</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Kritik & Saran</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Apotek Mitra</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">FAQ</a></li>
+                <div class="col-md-2 mb-4">
+                    <h6 class="body-2 mb-3" style="color: var(--neutral-oreo-light);">Medicastore</h6>
+                    <ul class="list-unstyled">
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Tentang Kami</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Syarat & Ketentuan</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Kebijakan & Privasi</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Kritik & Saran</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Apotek Mitra</a></li>
+                        <li><a href="#" class="body-1 text-white text-decoration-none">FAQ</a></li>
                     </ul>
                 </div>
 
                 <!-- Middle Section 2 -->
-                <div class="col-md-3 mb-4">
-                    <h6 class="fw-bold mb-3">Layanan Kami</h6>
+                <div class="col-md-2 mb-4">
+                    <h6 class="body-2 mb-3" style="color: var(--neutral-oreo-light);">Layanan Kami</h6>
                     <ul class="list-unstyled small">
-                        <li><a href="#" class="text-white text-decoration-none">Beli Obat</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Informasi Penyakit</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Informasi Dokter</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Informasi Fasilitas Kesehatan</a></li>
-                        <li><a href="#" class="text-white text-decoration-none">Artikel Kesehatan</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Beli Obat</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Informasi Penyakit</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Informasi Dokter</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Informasi Fasilitas Kesehatan</a></li>
+                        <li class="mb-3"><a href="#" class="body-1 text-white text-decoration-none">Artikel Kesehatan</a></li>
                     </ul>
                 </div>
 
                 <!-- Right Section -->
-                <div class="col-md-2 mb-4">
-                    <h6 class="fw-bold mb-3">Hubungi Kami</h6>
-                    <ul class="list-unstyled small">
-                        <li class="mb-2"><i class="bi bi-envelope"></i> support@medicastore.com</li>
-                        <li class="mb-2"><i class="bi bi-telephone"></i> 021-29293456</li>
-                        <li>
-                            <i class="bi bi-geo-alt"></i> 
-                            Jl Pospengumben Lama No. 2A,<br>
-                            Kembangan, Jakarta Barat,<br>
-                            DKI Jakarta, Indonesia
-                        </li>
-                    </ul>
+                <div class="col-md-3 mb-4">
+                    <h6 class="body-2 mb-3" style="color: var(--neutral-oreo-light);">Hubungi Kami</h6>
+
+                    <div class="d-flex gap-2 mb-3">
+                        <div>
+                            <icon name="envelope-simple" size="20" weight="fill"/>
+                        </div>
+                        <p class="subtitle-2 m-0">support@medicastore.com</p>
+                    </div>
+
+                    <div class="d-flex gap-2 mb-3">
+                        <div>
+                            <icon name="phone" size="20" weight="fill"/>
+                        </div>
+                        <p class="subtitle-2 m-0">021-29293456</p>
+                    </div>
+
+                    <div class="d-flex gap-2">
+                        <div>
+                            <icon name="map-pin" size="20" weight="fill"/>
+                        </div>
+                        <p class="subtitle-2 m-0">Jl Pospengumben Lama No. 2A,
+                            Kembangan, Jakarta Barat,
+                            DKI Jakarta, Indonesia</p>
+                    </div>
+
                 </div>
 
             </div>
@@ -72,7 +86,7 @@
         <!-- Copyright -->
         <div class="text-center mt-3 small" style="background-color:#143D73;">
             <div class="container py-2">
-                <div class="d-flex align-items-center justify-content-between">
+                <div class="body-2 d-flex align-items-center justify-content-between">
                     <span>Copyright © {{ new Date().getFullYear() }} by medicastore</span>
                     <span>PT. Clinisindo Putra Perkasa</span>
                 </div>
